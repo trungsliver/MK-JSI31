90b596bde89e2bd3435e3a1d0ab3f3fb27eec28e
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-07-03 19:46:30 +0700

    Lesson 4

---
+++ Lesson3/src/js/script.js
@@ -0,0 +1,28 @@
+// File firebase-config.js
+import { firebaseConfig } from "./firebase-config.js";
+// Thư viện Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+// Thư viện Authentication
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signInWithRedirect,
+    getRedirectResult,
+    signOut
+}
+    from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+// Thư viện Firestore
+import {
+    getFirestore,
+    doc,
+    setDoc,
+    getDoc,
+    collection,
+    getDocs,
+    query,
+    where
+}
+    from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
